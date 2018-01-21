b8f7c82a945825cb5b0517cae990599b920f82b8
schiz <geekle@yeah.net>
2018-01-21 15:57:01 +0800

    week2 challenge7

--- app.py
+++ app.py
@@ -1,40 +1,56 @@
-from flask import Flask,render_template,abort
-import os
-import json
+from datetime import datetime
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
+app.config.update(dict(SQLALCHEMY_DATABASE_URI='mysql://root@localhost/shiyanlou'))
 
-class Files(object):
-    def __init__(self):
-        self.result=self._read_files()
-    def _read_files(self):
-        result={}
-        for filename in os.listdir('/home/shiyanlou/files'):
-            file_path=os.path.join('/home/shiyanlou/files',filename)
-            with open(file_path) as f:
-                result[filename[:-5]]=json.load(f)
-        return result
-    def get_title_list(self):
-        return [item['title'] for item in self.result.values()]
-    def get_by_filename(self,filename):
-        return self.result.get(filename)
+db=SQLAlchemy(app)
+db.create_all()
 
-files=Files()
+class File(db.Model):
+    __tablename__='files'
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(80),unique=True)
+    created_time=db.Column(db.DateTime)
+    category_id=db.Column(db.Integer,db.ForeignKey('categories.id'))
+    category=db.relationship('Category',uselist=False)
+    content=db.Column(db.Text)
 
+    def __inti__(self,id,title,created_time,category,content):
+        self.id=id
+        self.title=title
+        self.created_time=created_time
+        self.content=content
+    
+class Category(db.Model):
+    __tablename__='categories'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(80))
+    files=db.relationship('File')
 
+    def __init__(self,name):
+        self.name=name
 
+def insert_datas():
+    java = Category('Java')
+    python = Category('Python')
+    file1 = File('Hello Java', datetime.utcnow(), java, 'File Content - Java is cool!')
+    file2 = File('Hello Python', datetime.utcnow(), python, 'File Content - Python is cool!')
+    db.session.add(java)
+    db.session.add(python)
+    db.session.add(file1)
+    db.session.add(file2)
+    db.session.commit()
 
 @app.route('/')
 def index():
-    title_list=files.get_title_list()
-    return render_template('index.html',title_list=title_list)
+    return render_template('index.html',files=File.query.all())
 
-@app.route('/files/<filename>')
-def file(filename):
-    fileobj=files.get_by_filename(filename)
-    if not fileobj:
-        abort(404)
-    return render_template('file.html',file_item=fileobj)
+@app.route('/files/<int:file_id>')
+def file(file_id):
+    file_item=File.query.get_or_404(file_id)    
+    return render_template('file.html',file_item=file_item)
 
 @app.errorhandler(404)
 def not_found(error):
